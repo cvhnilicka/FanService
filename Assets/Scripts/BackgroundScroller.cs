4a5201527ffb2c703f8c6f919360ded07fbfd2dc
Cormick <cvhnilicka@wisc.edu>
2021-05-26 15:42:49 -0700

    intial commit for fanservice

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float speed = 0f;
+    private Renderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset = new Vector2((speed * Time.time) % 1, 0f);
+    }
+}
